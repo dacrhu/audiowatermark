d33cb09792ea0c6a4ce2db9ce0a244f608d10cba
David Horvath <dacr@dacr.hu>
2023-03-11 15:38:22 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# audiowatermark
-Very simple analog audio watermark
+# Audiow watermark
+Very simple analog audio watermark generator.
+
+Dependecies:
+
+- Linux
+- Bash
+- sox (apt/yum install sox)
+
+Usage:
+
+```
+audiowatermark.sh input.wav output.wav rate_of_watermark_in_seconds
+```
+
+Example:
+
+```
+audiowatermark.sh mymusic.wav mymusic_wm.wav 10
+```
+
+(There will be a beep in the music every 10 seconds)
+
+
+I know this script is not performance effective. You can easily develop it further.
---
+++ audiowatermark.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+##################################################################################
+# Very simple analog audio watermark script.                                     #
+# Writen by David Horvath <dacr@dacr.hu>                                         #
+##################################################################################
+
+##################################################################################
+# MIT License                                                                    #
+#                                                                                #
+# Copyright (c) 2023 David Horvath <dacr@dacr.hu>                                #
+#                                                                                #
+# Permission is hereby granted, free of charge, to any person obtaining a copy   #
+# of this software and associated documentation files (the "Software"), to deal  #
+# in the Software without restriction, including without limitation the rights   #
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      #
+# copies of the Software, and to permit persons to whom the Software is          #
+# furnished to do so, subject to the following conditions:                       #
+#                                                                                #
+# The above copyright notice and this permission notice shall be included in all #
+# copies or substantial portions of the Software.                                #
+#                                                                                #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  #
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  #
+# SOFTWARE.                                                                      #
+##################################################################################
+
+INPUT=$1
+OUTPUT=$2
+WMRATE=$3
+
+if [ "$WMRATE" == "" ]; then
+        echo "###########################################################"
+        echo "Usage: $0 input.wav output.wav rate_of_watermark_in_seconds"
+        echo ""
+        echo "Example:"
+        echo "  $0 mymusic.wav mymusic_wm.wav 10"
+        echo "  (There will be a beep in the music every 10 seconds)"
+        echo "###########################################################"
+        exit -1
+fi
+
+sox -n -c2 -r 44100 wm.wav synth 1 sine 500
+cp $INPUT $OUTPUT
+
+LENGTH=$(sox $INPUT -n stat 2>&1 | grep Length | awk {'print $3'} | cut -d"." -f1)
+RATE=$(( $LENGTH / $WMRATE ))
+
+for i in $( eval echo {1..$RATE} ) ; do
+    START=$(( $i * $WMRATE ))
+    END=$(( $START + 1 ))
+    sox "|sox $OUTPUT -p trim 0 $START" \
+        wm.wav \
+        "|sox $INPUT -p trim $END $LENGTH" \
+        wmouttemp.wav
+    cp wmouttemp.wav $OUTPUT
+done
+
+rm wmouttemp.wav
+rm wm.wav
